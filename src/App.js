e93dad3f677d7b387b7e23ff61f0111e1c93d7d4
Shabaz123 <shahbaziub1377@gmail.com>
2024-01-04 16:37:26 +0500

    first comit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import react from "react"
+import Quiz from "./component/Quiz/Quiz"
+
+
+
+
+const App =()=> {
+
+
+
+
+
+
+return(
+<>
+
+
+<Quiz/>
+
+
+</>
+
+
+
+
+
+
+
+
+
+)
+
+
+
+
+}
+
+export default App
---
+++ src/assets/data.js
@@ -0,0 +1,53 @@
+export const data = [
+    {
+        question: "Which device is required for the internet connection ?",
+        option1: "Modem",
+        option2: "Router",
+        option3: "Lan Cable",
+        option4: "Pen Drive",
+
+        ans: 1,
+    },
+    {
+        question: "which continent has the heighest number of countries",
+        option1: "Asia",
+        option2: "Europe",
+        option3: "America",
+        option4: "Africa",
+
+        ans: 4,
+    },
+    {
+        question: "Junk e-mail is also called:",
+        option1: "Scame  ",
+        option2: "Fake",
+        option3: "Archive",
+        option4: "Bin",
+
+        ans: 1,
+    },
+    {
+        question: "A computer cannot Boot if it does not have the",
+        option1: "Application Software",
+        option2: "Internet",
+        option3: "Operating System",
+        option4: "Mouse",
+
+        ans: 3,
+    },
+    {
+        question: "First page of website is terms as?",
+        option1: "Index page",
+        option2: "Home page",
+        option3: "Sitemap",
+        option4: "Pen Drive",
+
+        ans: 2,
+    },
+
+
+
+
+
+
+]
